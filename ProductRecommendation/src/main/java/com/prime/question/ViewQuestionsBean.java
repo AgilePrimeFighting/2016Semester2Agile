80c2359e995ac3a4d8fba161a77493aafb2bcde2
Jatu-Phrom <jphr114@aucklanduni.ac.nz>
2016-09-28 04:56:11 +0700

    range list incompleted

--- ProductRecommendation/src/main/java/com/prime/question/ViewQuestionsBean.java
+++ ProductRecommendation/src/main/java/com/prime/question/ViewQuestionsBean.java
@@ -11,8 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.prime.customer.model.Customer;
+import com.prime.product.model.Product;
+import com.prime.product.service.ProductService;
 import com.prime.question.model.Question;
 import com.prime.question.service.QuestionService;
+import com.prime.weight.model.Weight;
 
 @Controller
 @Scope("request")
@@ -24,33 +28,56 @@ public class ViewQuestionsBean implements Serializable{
 
 	private static final Logger logger = Logger.getLogger(ViewQuestionsBean.class.getName());
 	
-
 	private List<Question> questions;
-	private List<RangeItem> rangeList;
-	
+	private List<RangeItem> ranges;
 	private Question selectedQuestion;
 	
 	@Autowired
 	private QuestionService questionService;
+	
+//	@Autowired
+//	private WeightService weightService;
+	
+	@Autowired
+	private ProductService productService;
 	
 	@PostConstruct
 	public void init(){
 		logger.info("initiating");
 		
 		questions = new ArrayList<Question>();
-		rangeList = new ArrayList<RangeItem>();
+		ranges = new ArrayList<RangeItem>();
 		
 		if(questionService != null) questions = questionService.listAll();
 		else logger.info("question service is null");
 		
-		
-		
-		//RangeItem range = new RangeItem("EXO",10,15,"true");
-		//rangeList.add(range);
 		//TODO set rangeList
+//		List<Product> products = productService.listAll();
+//		for(Product product : products){
+//			List<Weight> weights = weightService.getWeighsFromProductId(product.getProductID());
+//			boolean isFirst = true;
+//			int min = 0;
+//			int max = 0;
+//			for(Weight weight : weights){
+//				int value = weight.getWeightValue();
+//				if(isFirst){
+//					min = value;
+//					max = value;
+//					isFirst = false;
+//				}
+//				if(value < min) min = value;
+//				if(value > max) max = value;
+//			}
+//			RangeItem range = new RangeItem(product.getProductName(),min,max,product.outputActive());
+//			ranges.add(range);
+//		}
 		
 		
 	}
+	
+//	public List<Weight> getWeighsFromProductId(int id){
+//		return em.createQuery("SELECT w FROM Weight WHERE w.PRODUCT_ID = " + id , Weight.class).getResultList();
+//	}
 
 	public void onDelete(Question question){
 		logger.info("deleting question");
@@ -85,11 +112,11 @@ public class ViewQuestionsBean implements Serializable{
 	}
 
 	public List<RangeItem> getRangeList() {
-		return rangeList;
+		return ranges;
 	}
 
-	public void setRangeList(List<RangeItem> rangeList) {
-		this.rangeList = rangeList;
+	public void setRangeList(List<RangeItem> ranges) {
+		this.ranges = ranges;
 	}
 
 }
