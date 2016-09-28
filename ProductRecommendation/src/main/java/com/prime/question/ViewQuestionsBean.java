76abbd72a018bece39e38421b073d90adb40ea9c
RachaelYuanRachaelYuan <Yuen_sheffield@163.com>
2016-09-28 18:22:00 +0800

    Rename AddProduct.xhtml, set up range item list

--- ProductRecommendation/src/main/java/com/prime/question/ViewQuestionsBean.java
+++ ProductRecommendation/src/main/java/com/prime/question/ViewQuestionsBean.java
@@ -24,89 +24,119 @@ import com.prime.weight.service.WeightService;
 @Controller
 @Scope("request")
 public class ViewQuestionsBean implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	private static final Logger logger = Logger.getLogger(ViewQuestionsBean.class.getName());
+    private static final Logger logger = Logger.getLogger(ViewQuestionsBean.class.getName());
 
-	private List<Question> questions;
-	private List<RangeItem> rangeList;
-	Map<Integer, HashMap<Integer, Weight>> weightMap = new HashMap<Integer, HashMap<Integer, Weight>>();
-	private List<Product> products;
-	@Autowired
-	private QuestionService questionService;
-	@Autowired
-	private ProductService productService;
-	
-	@PostConstruct
-	public void init() {
-		logger.info("initiating");
-		products = productService.listAll();
-		setRangeList(new ArrayList<RangeItem>());
+    private List<Question> questions;
+    private List<RangeItem> rangeList;
+    Map<Integer, HashMap<Integer, Weight>> weightMap = new HashMap<Integer, HashMap<Integer, Weight>>();
+    private List<Product> products;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private ProductService productService;
 
-		if (questionService != null) {
-			questions = questionService.listAll();
-		} else {
-			logger.info("question service is null");
-		}
-		// RangeItem range = new RangeItem("EXO",10,15,"true");
-		// rangeList.add(range);
-		// TODO set rangeList
-		
-		for(Question question : questions){
-			for(Option option: question.getOptions()){
-				HashMap<Integer, Weight> productIdToWeightMap = new HashMap<Integer, Weight>();
-				weightMap.put(option.getOptionId(), productIdToWeightMap);
-				for(Weight weight : option.getWeightList()){
-					productIdToWeightMap.put(weight.getProductId(), weight);
-				}
-			}
-		}
+    @PostConstruct
+    public void init() {
+        logger.info("initiating");
+        products = productService.listAll();
+        setRangeList(new ArrayList<RangeItem>());
 
-	}
-	
-	public Weight findWeight(Integer optionId, Integer productId ){
-		Weight weight =  weightMap.get(optionId).get(productId);
-		return weight;
-	}
+        if (questionService != null) {
+            questions = questionService.listAll();
+        } else {
+            logger.info("question service is null");
+        }
 
-	public void onDelete(Question question) {
-		logger.info("deleting question");
-		questions.remove(question);
-		questionService.delete(question);
-	}
+        for (Question question : questions) {
+            for (Option option : question.getOptions()) {
+                List<Weight> weightList = option.getWeightList();
+                int min = minWeight(weightList);
+                int max = maxWeight(weightList);
+                for (Weight weight : weightList) {
+                    Product product = weight.getProduct();
+                    RangeItem range = new RangeItem(product.getProductName(), min, max, product.isProductActive() + "");
+                    rangeList.add(range);
+                }
+            }
+        }
 
-	public List<Question> getQuestions() {
-		return questions;
-	}
+        for (Question question : questions) {
+            for (Option option : question.getOptions()) {
+                HashMap<Integer, Weight> productIdToWeightMap = new HashMap<Integer, Weight>();
+                weightMap.put(option.getOptionId(), productIdToWeightMap);
+                for (Weight weight : option.getWeightList()) {
+                    productIdToWeightMap.put(weight.getProductId(), weight);
+                }
+            }
+        }
 
-	public void setQuestions(List<Question> questions) {
-		this.questions = questions;
-	}
+    }
 
-	public QuestionService getQuestionService() {
-		return questionService;
-	}
+    private int minWeight(List<Weight> weightList) {
+        if (weightList == null || weightList.isEmpty()) return 0;
+        int min = weightList.get(0).getWeightValue();
+        for (int i = 1; i < weightList.size(); i++) {
+            Weight w = weightList.get(i);
+            if (w.getWeightValue() < min) min = w.getWeightId();
+        }
+        return min;
+    }
 
-	public void setQuestionService(QuestionService questionService) {
-		this.questionService = questionService;
-	}
+    private int maxWeight(List<Weight> weightList) {
+        if (weightList == null || weightList.isEmpty()) return 0;
+        int max = weightList.get(0).getWeightValue();
+        for (int i = 1; i < weightList.size(); i++) {
+            Weight w = weightList.get(i);
+            if (w.getWeightValue() > max) max = w.getWeightId();
+        }
+        return max;
+    }
 
-	public List<RangeItem> getRangeList() {
-		return rangeList;
-	}
+    public Weight findWeight(Integer optionId, Integer productId) {
+        Weight weight = weightMap.get(optionId).get(productId);
+        return weight;
+    }
 
-	public void setRangeList(List<RangeItem> rangeList) {
-		this.rangeList = rangeList;
-	}
+    public void onDelete(Question question) {
+        logger.info("deleting question");
+        questions.remove(question);
+        questionService.delete(question);
+    }
 
-	public List<Product> getProducts() {
-		return products;
-	}
+    public List<Question> getQuestions() {
+        return questions;
+    }
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
-	}
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public QuestionService getQuestionService() {
+        return questionService;
+    }
+
+    public void setQuestionService(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    public List<RangeItem> getRangeList() {
+        return rangeList;
+    }
+
+    public void setRangeList(List<RangeItem> rangeList) {
+        this.rangeList = rangeList;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
