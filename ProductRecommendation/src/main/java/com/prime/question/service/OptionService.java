50e748be725ba494dd4be9323dc8eb2a87bdcbb5
JackBlack <jzha471@aucklanduni.ac.nz>
2016-09-29 00:31:46 +1300

    Create question with default weight , deleting questions with weight.

--- ProductRecommendation/src/main/java/com/prime/question/service/OptionService.java
+++ ProductRecommendation/src/main/java/com/prime/question/service/OptionService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.prime.product.service.ProductService;
 import com.prime.question.model.Option;
 
 @Service
@@ -14,9 +16,14 @@ public class OptionService {
 
 	@PersistenceContext
 	private EntityManager em;
+	
+
 
 	public List<Option> listAll() {
 		return em.createQuery("SELECT u FROM Option u", Option.class).getResultList();
 	}
+	
+	
+	
 
 }
