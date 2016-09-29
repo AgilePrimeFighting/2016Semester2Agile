d269226a97ad19e6f3cb1b7c0c1bfe9e58cfc39e
Jatu-Phrom <jphr114@aucklanduni.ac.nz>
2016-09-29 20:30:26 +0700

    Add product overview input in create new product page

--- ProductRecommendation/src/main/java/com/prime/product/CreateProductBean.java
+++ ProductRecommendation/src/main/java/com/prime/product/CreateProductBean.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.prime.product.model.Product;
 import com.prime.product.service.ProductService;
 import com.prime.question.CreateQuestionBean;
 
@@ -25,88 +26,51 @@ public class CreateProductBean
 	@Autowired
 	private ProductService productService;
 	
-	private String productName ; 
-	private boolean productActive ;
-	private boolean productTrial ;
-	private String productURL ;
+	private Product product;
 
-	public void initBean () 
-	{
-		System.out.println("start to init product bean");
-		productName = null ;
-		productActive = false ;
-		//productTrail  = false ;
-		productTrial = false ;
-		productURL = null ;
+	public void initBean (){
+		product = new Product();
+		product.setProductName(null);
+		product.setProductActive(false);
+		product.setProductTrial(false);
+		product.setProductURL(null);
+		product.setOverview(null);
 	}
 
 	
 	public String onSave(){
 		logger.info("onSave");
-		//logger.info("question body = " + getQuestionBody());
-		productService.createNewProduct(productName, productActive ,productTrial , productURL);
+		product = productService.createNewProduct(product);
 		initBean();
-		//return "ViewProducts" ;
 		return "ViewProducts?faces-redirect=true";
+	}
+	
+	public void validateURL (FacesContext context , UIComponent component , Object value ) throws ValidatorException{
+		if ( product.isProductTrial() 
+				&& (value == null || value.toString().compareTo("") == 0 )) {
+			FacesMessage message = new FacesMessage ("This is a trial product , you must give a URL");
+			throw new ValidatorException(message) ; 
+		}
+	}
+	
+	public static Logger getLogger() {
+		return logger;
 	}
 	
 	public ProductService getProductService() {
 		return productService;
 	}
+	
 	public void setProductService(ProductService productService) {
 		this.productService = productService;
 	}
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public boolean isProductActive() {
-		return productActive;
-	}
-	public void setProductActive(boolean productActive) {
-		this.productActive = productActive;
+
+	public Product getProduct() {
+		return product;
 	}
 
-
-	public boolean isProductTrial() {
-		return productTrial;
-	}
-
-
-	public void setProductTrial(boolean productTrial) {
-		this.productTrial = productTrial;
-	}
-
-
-	public String getProductURL() {
-		return productURL;
-	}
-
-
-	public void setProductURL(String productURL) {
-		this.productURL = productURL;
-	}
-
-
-	public static Logger getLogger() {
-		return logger;
-	}
-	
-
-	public void validateURL (FacesContext context , UIComponent component , Object value ) throws ValidatorException
-
-	{
-
-		System.out.println("start to validate URL ");
-		System.out.println("URL value " + value.toString()  + " trial value " + productTrial);
-		//String data = value.toString() ;
-		if ( productTrial == true && (value == null || value.toString().compareTo("") == 0 )) 
-		{
-			FacesMessage message = new FacesMessage ("This is a trial product , you must give a URL");
-			throw new ValidatorException(message) ; 
-		}
+	public void setProduct(Product product) {
+		this.product = product;
 	}
 	
 }
