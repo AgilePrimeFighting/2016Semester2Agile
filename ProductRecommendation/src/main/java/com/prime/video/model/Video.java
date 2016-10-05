7d1e7451c8b7c4f57b43dfd455ffc6e467fec774
jche764 <jche764@aucklanduni.ac.nz>
2016-10-05 23:24:44 +1300

    Add video back end/ Video demo

--- ProductRecommendation/src/main/java/com/prime/video/model/Video.java
+++ ProductRecommendation/src/main/java/com/prime/video/model/Video.java
@@ -15,29 +15,28 @@ import com.prime.product.model.Product;
 @Entity
 @Table
 public class Video {
-	
+
 	@Id
 	@Column(name = "VIDEO_ID")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer videoId;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "PRODUCT_ID")
-	private Product product;
-	
+
 	@Column(name = "VIDEO_NAME")
 	private String videoName;
-	
+
 	@Column(name = "VIDEO_URL")
 	private String videoUrl;
-	
+
 	@Column(name = "VIDEO_DESCRIPTION")
 	private String videoDescription;
-	
+
 	@Column(name = "VIDEO_LENGTH")
 	private Integer videoLength;
 
-	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PRODUCT_ID", insertable = true, updatable = true)
+	private Product product;
+
 	public Integer getVideoId() {
 		return videoId;
 	}
@@ -45,7 +44,7 @@ public class Video {
 	public void setVideoId(Integer videoId) {
 		this.videoId = videoId;
 	}
-	
+
 	public Product getProduct() {
 		return product;
 	}
@@ -53,7 +52,7 @@ public class Video {
 	public void setVideoProduct(Product product) {
 		this.product = product;
 	}
-	
+
 	public String getVideoName() {
 		return videoName;
 	}
@@ -61,7 +60,7 @@ public class Video {
 	public void setVideoName(String videoName) {
 		this.videoName = videoName;
 	}
-	
+
 	public String getVideoUrl() {
 		return videoUrl;
 	}
@@ -69,7 +68,7 @@ public class Video {
 	public void setVideoUrl(String videoUrl) {
 		this.videoUrl = videoUrl;
 	}
-	
+
 	public String getVideoDescription() {
 		return videoDescription;
 	}
@@ -77,7 +76,7 @@ public class Video {
 	public void setVideoDescription(String videoDescription) {
 		this.videoDescription = videoDescription;
 	}
-	
+
 	public Integer getVideoLength() {
 		return videoLength;
 	}
